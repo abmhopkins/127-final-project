47d8973e2f310e1921849bb069a1317502743883
abmhopkins <abmhopkins@gmail.com>
2020-04-24 19:15:29 -0400

    Added the beginning of the start screen.

--- src/caveEscape.java
+++ src/caveEscape.java
@@ -6,19 +6,41 @@ import comp127graphics.CanvasWindow;
 import comp127graphics.ui.Button;
 import java.awt.Color;
 
-
-
 public class caveEscape {
     private CanvasWindow canvas;
     private stillGoingScenarios stillGoingScenarios;
 
+    private static final int CANVAS_WIDTH = 750;
+    private static final int CANVAS_HEIGHT = 500;
+
+
+    private GraphicsText story;
+
     public caveEscape() {
-        canvas = new CanvasWindow("Cave Escape", 500, 500);
+        canvas = new CanvasWindow("Cave Escape", CANVAS_WIDTH, CANVAS_HEIGHT);
         canvas.setBackground(Color.WHITE);
         Button startGameButton = new Button("Start Game");
-        startGameButton.setCenter(250,400);
+
+        startGameButton.setCenter(CANVAS_WIDTH*0.5,CANVAS_HEIGHT*0.5);
+        startGameButton.onClick(() -> startGame());
         canvas.add(startGameButton);
         }
+
+    public void startGame(){
+        canvas.removeAll();
+        story = new GraphicsText("You awake in a dark room. What do you want to do: Feel Around or Do Nothing?");
+        story.setCenter(CANVAS_WIDTH*0.5, CANVAS_HEIGHT*0.33);
+        canvas.add(story);
+
+        Button feelAround = new Button("Feel Around");
+        feelAround.setCenter(CANVAS_WIDTH*0.5,CANVAS_HEIGHT*0.6);
+        canvas.add(feelAround);
+
+        Button doNothing = new Button("Do Nothing");
+        doNothing.setCenter(CANVAS_WIDTH*0.5,CANVAS_HEIGHT*0.7);
+        canvas.add(doNothing);
+
+    }
 
     public static void main(String[] args) {
         new caveEscape();
