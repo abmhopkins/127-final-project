f209457dac9cc75574175591cff33576a7f29fcc
abmhopkins <abmhopkins@gmail.com>
2020-04-27 11:08:44 -0400

    Added a canvasHelper, helps minimizes duplicated code.

--- src/caveEscape.java
+++ src/caveEscape.java
@@ -12,6 +12,8 @@ public class caveEscape {
 
     private static final int CANVAS_WIDTH = 750;
     private static final int CANVAS_HEIGHT = 500;
+    private Button firstChoice;
+    private Button secondChoice;
 
 
     private GraphicsText story;
@@ -22,25 +24,41 @@ public class caveEscape {
         Button startGameButton = new Button("Start Game");
 
         startGameButton.setCenter(CANVAS_WIDTH*0.5,CANVAS_HEIGHT*0.5);
-        startGameButton.onClick(() -> startGame());
+        startGameButton.onClick(this::startGame);
         canvas.add(startGameButton);
         }
 
     public void startGame(){
+        canvasHelper("You awake in a dark room. What do you want to do: Feel Around or Do Nothing?",
+                "Feel Around",
+                "Do Nothing");
+        firstChoice.onClick(this::feelAround);
+
+    }
+
+    public void feelAround(){
+        canvasHelper("You feel the ground around you. You find a backpack and a flashlight. " +
+                "Do you want to turn on the flashlight or check your backpack?",
+                "Flashlight",
+                "Backpack");
+
+    }
+
+    public void canvasHelper(String storyText, String choice1, String choice2){
         canvas.removeAll();
-        story = new GraphicsText("You awake in a dark room. What do you want to do: Feel Around or Do Nothing?");
+        story = new GraphicsText(storyText);
         story.setCenter(CANVAS_WIDTH*0.5, CANVAS_HEIGHT*0.33);
         canvas.add(story);
 
-        Button feelAround = new Button("Feel Around");
-        feelAround.setCenter(CANVAS_WIDTH*0.5,CANVAS_HEIGHT*0.6);
-        canvas.add(feelAround);
+        firstChoice = new Button(choice1);
+        firstChoice.setCenter(CANVAS_WIDTH*0.5,CANVAS_HEIGHT*0.6);
+        canvas.add(firstChoice);
 
-        Button doNothing = new Button("Do Nothing");
-        doNothing.setCenter(CANVAS_WIDTH*0.5,CANVAS_HEIGHT*0.7);
-        canvas.add(doNothing);
-
+        secondChoice = new Button(choice2);
+        secondChoice.setCenter(CANVAS_WIDTH*0.5,CANVAS_HEIGHT*0.7);
+        canvas.add(secondChoice);
     }
+
 
     public static void main(String[] args) {
         new caveEscape();
