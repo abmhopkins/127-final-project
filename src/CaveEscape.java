85881dfb62295c3729b38addb56112ff49d6ee61
abmhopkins <abmhopkins@gmail.com>
2020-04-27 18:45:15 -0400

    Moved the organizational functions to the StoryHelper class.

---
+++ src/CaveEscape.java
@@ -0,0 +1,69 @@
+import comp127graphics.*;
+
+import comp127graphics.CanvasWindow;
+import comp127graphics.ui.Button;
+import java.awt.Color;
+
+public class CaveEscape {
+    private CanvasWindow canvas;
+    private stillGoingScenarios stillGoingScenarios;
+
+    private static final int CANVAS_WIDTH = 750;
+    private static final int CANVAS_HEIGHT = 500;
+
+    private StoryHelper storyHelper;
+    private LeftTunnelAlive leftTunnel;
+
+    public CaveEscape() {
+        canvas = new CanvasWindow("Cave Escape", CANVAS_WIDTH, CANVAS_HEIGHT);
+        canvas.setBackground(Color.WHITE);
+        Button startGameButton = new Button("Start Game");
+
+        startGameButton.setCenter(CANVAS_WIDTH*0.5,CANVAS_HEIGHT*0.5);
+        startGameButton.onClick(this::startGame);
+        canvas.add(startGameButton);
+
+        storyHelper = new StoryHelper(canvas);
+
+        leftTunnel = new LeftTunnelAlive(canvas);
+        }
+
+    public void startGame(){
+        storyHelper.canvasHelper("You awake in a dark room. What do you want to do: Feel Around or Do Nothing?",
+                "Feel Around",
+                "Do Nothing");
+        storyHelper.firstChoice.onClick(this::feelAround);
+        storyHelper.secondChoice.onClick(this::doNothing);
+    }
+
+    private void feelAround(){
+        storyHelper.canvasHelper("You feel the ground around you. You find a backpack and a flashlight. " +
+                "Do you want to turn on the flashlight or check your backpack?",
+                "Flashlight",
+                "Backpack");
+        storyHelper.firstChoice.onClick(this::flashlight);
+//        secondChoice.onClick();
+    }
+
+    public void doNothing(){
+        storyHelper.canvasHelper("You sit there on the cold dark floor and do nothing. That wasn't much fun was it?" +
+                        "GAME OVER",
+                "Restart",
+                "null");
+        storyHelper.firstChoice.onClick(this::startGame);
+    }
+
+    private void flashlight(){
+        storyHelper.canvasHelper("Your fingers stumble over the flashlight searching for the button. " +
+                "You find it and the path in front of you is illuminated. There are two tunnels, a left " +
+                "tunnel and a right tunnel.",
+                "Left Tunnel",
+                "Right Tunnel");
+        storyHelper.firstChoice.onClick(() -> leftTunnel.leftTunnel());
+    }
+
+
+    public static void main(String[] args) {
+        new CaveEscape();
+    }
+}
