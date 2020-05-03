614a66dea14e1efee7b867284b0bff48281da95c
101082299 <ajohal@macalester.edu>
2020-05-03 15:50:02 -0500

    Added a picture for the title screen and put some commenting for Cave Escape and StartGame

--- src/CaveEscape.java
+++ src/CaveEscape.java
@@ -12,14 +12,19 @@ public class CaveEscape {
 
     private StartGame startGame;
 
+
+    /*This is the main class of the game Cave Escape. The constructor holds the details needed for the initial
+     * starting page of the game. It then calls the start game method when the button is pressed to put the user
+     * inside the game and begin their adventure.*/
     public CaveEscape() {
         canvas = new CanvasWindow("Cave Escape", CANVAS_WIDTH, CANVAS_HEIGHT);
         canvas.setBackground(Color.WHITE);
-
+        Image image = new Image(0, 0);
+        image.setImagePath("startGamePhotos/titleScreenPic.png");
+        canvas.add(image);
         startGame = new StartGame(canvas);
-
         Button startGameButton = new Button("Start Game");
-        startGameButton.setCenter(CANVAS_WIDTH*0.5,CANVAS_HEIGHT*0.5);
+        startGameButton.setCenter(CANVAS_WIDTH*0.5,CANVAS_HEIGHT*0.8);
         startGameButton.onClick(startGame::startGame);
         canvas.add(startGameButton);
         canvas.draw();
--- src/StartGame.java
+++ src/StartGame.java
@@ -4,42 +4,48 @@ public class StartGame {
 
     private StoryHelper storyHelper;
     private LeftTunnelAlive leftTunnel;
-    private RightTunnelAlive rightTunnel; //Amrit
+    private RightTunnelAlive rightTunnel;
+
+    /*This is the class that deals with the first portion of the game. After the uses chooses to start a game
+    * they will be put in the scenario below until they are asked to choose from either a right tunnel or left tunnel.
+    * Afterwards, this class will ridirect them into one of the two scenarios and they will attempt to reach one of the
+    * four true endings or any of the others*/
 
     public StartGame(CanvasWindow canvas){
         this.storyHelper = new StoryHelper(canvas);
         leftTunnel = new LeftTunnelAlive(canvas);
-        rightTunnel = new RightTunnelAlive(canvas); //Amrit
+        rightTunnel = new RightTunnelAlive(canvas);
     }
-
+    //This start of the game
     public void startGame(){
-        storyHelper.canvasHelper("You awake in a dark room. What do you want to do: Feel Around or Do Nothing?",
+        storyHelper.canvasHelper("You awake in a dark space and are confused, it feels damp and smells. " +
+                        "Is this heaven? ",
                 "Feel Around",
                 "Do Nothing",
                 "startGamePhotos/dark-floor.png");
-        //TODO image
         storyHelper.firstChoice.onClick(this::feelAround);
         storyHelper.secondChoice.onClick(this::doNothing);
     }
-
+    //This is what will help you see the rest of the game
     private void feelAround(){
-        storyHelper.canvasHelper("You feel the ground around you. You find a flashlight. " +
-                        "Do you want to turn on the flashlight?",
+        storyHelper.canvasHelper("You feel the ground around you. You find a flashlight (or a torch if you lost" +
+                        " the revolutionary war). " +  "Do you want to turn on the flashlight/torch?",
                 "Flashlight",
                 "null",
                 "startGamePhotos/flashlight.png");
         storyHelper.firstChoice.onClick(this::flashlight);
-//        secondChoice.onClick();
     }
-
+    //This class is one of the first deaths and if choosen will direct you to try again
     public void doNothing(){
-        storyHelper.canvasHelper("You sit there on the cold dark floor and do nothing. That wasn't much fun was it?",
+        storyHelper.canvasHelper("You sit there on the cold dark floor and do nothing. That wasn't heaven," +
+                        " but after starving you're definitely close. R.I.P",
                 "Restart",
                 "null",
                 "startGamePhotos/dark-floor.png");
         storyHelper.firstChoice.onClick(this::startGame);
     }
-
+    //This is the first major decision of the game that will ask the user which branch
+    //and path they would like to follow.
     private void flashlight(){
         storyHelper.canvasHelper("Your fingers stumble over the flashlight searching for the button. " +
                         "You find it and the path in front of you is illuminated. There are two tunnels, a left " +
