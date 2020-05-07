5eae5c24202fd4fe5440ef24e8b22b91139b3088
abmhopkins <abmhopkins@gmail.com>
2020-05-06 22:39:23 -0400

    Added a heading javadoc

--- src/CaveEscape.java
+++ src/CaveEscape.java
@@ -4,6 +4,12 @@ import comp127graphics.CanvasWindow;
 import comp127graphics.ui.Button;
 import java.awt.Color;
 
+/**
+ * The main class of the cave escape game, displays a canvas with the start screen.
+ * <p>
+ * Author: Amrit Johal and Alexander Hopkins
+ * Original Work
+ */
 public class CaveEscape {
     private CanvasWindow canvas;
 
@@ -13,9 +19,11 @@ public class CaveEscape {
     private StartGame startGame;
 
 
-    /*This is the main class of the game Cave Escape. The constructor holds the details needed for the initial
+    /**
+     * This is the main class of the game Cave Escape. The constructor holds the details needed for the initial
      * starting page of the game. It then calls the start game method when the button is pressed to put the user
-     * inside the game and begin their adventure.*/
+     * inside the game and begin their adventure.
+     */
     public CaveEscape() {
         canvas = new CanvasWindow("Cave Escape", CANVAS_WIDTH, CANVAS_HEIGHT);
         canvas.setBackground(Color.WHITE);
